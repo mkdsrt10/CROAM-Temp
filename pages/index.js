89381639235a10661d506b474c25ade096a02a00
kushaana <65369802+kushaana@users.noreply.github.com>
2021-03-27 14:08:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,43 +4,41 @@ import { ThemeProvider } from "styled-components";
 import Main from "../src/components/main";
 
 import Navigation from "sections/Navigation";
-import Banner from "sections/Banner-default";
-import About from "sections/About";
-import Features from "sections/Features";
-import Faq from "sections/Faq";
+import Banner from "sections/Banner";
+import AboutClassic from "sections/About-classic";
+import FeaturesClassic from "sections/Features-classic";
+import FaqClassic from "sections/Faq-Classic";
 import Integrations from "sections/Integrations";
-import AppScreens from "sections/AppScreens";
-import Counters from "sections/Counters";
+import AppScreens from "sections/AppScreens-classic";
+import Counter from "sections/Counters";
 import Pricing from "sections/Pricing";
 import Testimonial from "sections/Testimonial";
 import News from "sections/News";
 import Getapp from "sections/Getapp";
-import Contact from "sections/Contact";
-import Footer from "sections/Footer";
+import Client from "sections/Client";
+import Footer from "sections/Footer-classic";
+import Contact from "sections/Contact"
 
 import { GlobalStyle } from "sections/app.style";
-import theme from "theme/app/themeStyles";
+import theme from "theme/classic/themeStyles";
 
 const Home = () => (
   <ThemeProvider theme={theme}>
-    <Main title="Appion React Gatsby nextjs landing">
+    <Main title="CRoam">
       <GlobalStyle />
       <Navigation />
       <Banner />
-      <About />
-      <Integrations />
-      <Features />
       <AppScreens />
-      <Counters />
-      <Pricing />
+      <AboutClassic />
+      <FeaturesClassic />
       <Testimonial />
-      <Faq />
+      <Counter />
       <Getapp />
       <News />
+      <Client />
       <Contact />
       <Footer />
     </Main>
   </ThemeProvider>
 );
-
 export default Home;
